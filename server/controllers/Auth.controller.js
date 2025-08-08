bdacda99e49270800bd994e4384d157a3b1a4429
Fenetroba <fenetroba700@gmail.com>
2025-08-08 21:36:48 +0300

    Update Auth.controller.js

--- server/controllers/Auth.controller.js
+++ server/controllers/Auth.controller.js
@@ -50,7 +50,7 @@ export const CreateUser = async (req, res) => {
     });
 res.cookie("token", token, {
       httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
+      secure: true,
       maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
       samesite: "Strict",
     });
@@ -85,7 +85,7 @@ export const LoginUser = async (req, res) => {
     });
 res.cookie("token", token, {
       httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
+      secure: true,
       maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
       samesite: "Strict",
     });
