8eb9ccf9718e2b45de4518e97f5dec82c605471e
Fenetroba <fenetroba700@gmail.com>
2025-08-08 22:38:04 +0300

    Update Auth.controller.js

--- server/controllers/Auth.controller.js
+++ server/controllers/Auth.controller.js
@@ -52,7 +52,7 @@ res.cookie("token", token, {
       httpOnly: true,
       secure: true,
       maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
-      samesite: "Strict",
+      sameSite: 'none',
     });
 
      // Return success response with token
@@ -87,7 +87,7 @@ res.cookie("token", token, {
       httpOnly: true,
       secure: true,
       maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
-      samesite: "Strict",
+      sameSite: 'none'
     });
 
      // Return success response with token
